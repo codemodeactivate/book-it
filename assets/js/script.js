02673d6b53587e2a78e2339cf7adaf8aef03cb6d
cschmidt216 <iamdrainy@gmail.com>
2023-04-06 15:51:54 -0700

    Merge branch 'main' of github.com:codemodeactivate/cwru-project-one into local-storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -127,6 +127,24 @@ const populateModal = (book) => {
     modalImage.src = image;
     modalGenre.textContent = genre;
     modalSynopsis.textContent = synopsis;
+
+    const addToListButton = document.querySelector('.addBtn');//listener for add to list button
+    addToListButton.addEventListener('click', () => {
+    let booksLocal = JSON.parse(localStorage.getItem('booksLocal')) || [];//defines array
+    const books = {title: title, author: author, genre: genre, synopsis: synopsis, cover:image};//obj that goes in array
+    booksLocal.push(books);//add it too arrray
+    localStorage.setItem('booksLocal', JSON.stringify(booksLocal));//store
+    });
+    const removeListButton = document.querySelector('.removeBtn');
+    removeListButton.addEventListener('click', () => {
+        let booksLocal = JSON.parse(localStorage.getItem('booksLocal')) || [];//defines array
+        const toRemove = booksLocal.findIndex(books => books.title === title);// search for the object
+        // remove from array
+        if (toRemove !== -1) {
+        booksLocal.splice(toRemove, 1);
+        }
+        localStorage.setItem('booksLocal', JSON.stringify(booksLocal));  
+    })
 };
 
 
